de9284aa61cb1742bd7df0c879690e58572674bd
william <williamwambugu663@gmail.com>
2025-04-25 13:56:11 +0300

    clear button

--- src/App.jsx
+++ src/App.jsx
@@ -112,6 +112,7 @@ function App() {
           {isSending ? 'Sending...' : 'Send'}
         </button>
       </div>
+      <button className= "clear-button" onClick={() => setMessages([])}>Clear Chat</button>
     </div>
   );
 }
